8be510e4e5ae435c356d7163dea2457278d8b1a5
tuan2123 <huvovutru@gmail.com>
2024-10-13 19:37:09 +0700

    first commit

---
+++ thiep-mung-main/config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Cô nương';
+const giftUrl = 'http:google.com'
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng Cô Nương';
+const contentCard = 'Chúc cô Nương 20/10 tràn ngập niềm vui và những nụ cười. Mong điều tốt đẹp nhất sẽ đến với cô trong hôm nay và cả những ngày sau, yêu cô nương nhiều!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'co nuong 4.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
